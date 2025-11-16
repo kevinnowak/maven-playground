1089de8b16490a84deb5a6bd6b1d99cccb83df07
Kevin Nowak <kevin.nowak@studium.fernuni-hagen.de>
2025-11-17 00:26:24 +0100

    Update

---
+++ quickstart/src/main/java/com/github/kevinnowak/App.java
@@ -0,0 +1,12 @@
+package com.github.kevinnowak;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
